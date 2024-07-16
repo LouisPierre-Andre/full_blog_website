89af603d1daef0d07a95870ac1d6fa03e61ec8f7
unknown <Louis Pierre Andre>
2024-07-16 12:51:26 -0400

    Install tailwing css

--- my_blog/src/App.jsx
+++ my_blog/src/App.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Dashboard from './pages/Dashboard'
+import Signin from './pages/Signin'
+import Signup from './pages/Signup'
+import Project from './pages/Project'
 
+    
 function App() {
   return (
-    <div>
-      <h1 className='text-3xl text-red-800'>Welcom to the project</h1>
-    </div>
+      <BrowserRouter>
+          <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/about' element={<About />} />
+              <Route path='/dashboard' element={<Dashboard />} />
+              <Route path='/signin' element={<Signin />} />
+              <Route path='/signup' element={<Signup />} />
+              <Route path='/projects' element={<Project />} />
+
+          </Routes>
+      </BrowserRouter>
+      
   )
 }
 
