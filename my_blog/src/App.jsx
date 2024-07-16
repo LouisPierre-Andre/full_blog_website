729363514f918a5543ce302b43237223a102b517
unknown <Louis Pierre Andre>
2024-07-16 12:31:46 -0400

    install tailwind css

---
+++ my_blog/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1 className='text-3xl text-red-800'>Welcom to the project</h1>
+    </div>
+  )
+}
+
+export default App
